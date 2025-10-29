dd232ba35e21d60da057b56946817fe05dbc9efa
mobdev32 <mobdev@kolabnow.com>
2025-10-29 13:50:05 +0400

    Tweaked materials UI

---
+++ main/templatetags/custom_filters.py
@@ -0,0 +1,11 @@
+import markdown
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+@register.filter(name='markdown')
+def markdown_filter(text):
+    md = markdown.Markdown(extensions=['extra', 'codehilite'])
+    return mark_safe(md.convert(text))
+
